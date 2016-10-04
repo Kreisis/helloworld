b37b3c6df559514be9185f3f84149ba560060196
Karolis <kreisis4@gmail.com>
2016-10-04 17:06:47 +0300

    Pirma programa

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveiki visi!");
+            Console.WriteLine("Spauskite bet kurį klavišą");
+            Console.ReadLine();
+        }
+    }
+}
